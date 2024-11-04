33b8726533f4fd6a14a337e79feac0c704034fc9
TeoEchavarria <echavarria.0042@gmail.com>
2024-11-03 22:39:21 -0500

    Add function to retrieve company share changes based on time

---
+++ src/services/company_information.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+
+def companies_data(time):
+    sheet_id = os.getenv("CHANGES_COMPANY_SHARES_DATA")
+    users_responses = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid=766765566")
+
+    data_list = users_responses.to_dict(orient='records')
+    return data_list
+
+    
