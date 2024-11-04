45f87cfb550c273fff09fcc5cc7c14a1e0111a5e
TeoEchavarria <echavarria.0042@gmail.com>
2024-11-04 02:30:05 -0500

    Refactor data_processing.py to use environment variable for sheet ID and update portfolio handling

--- src/services/data_processing.py
+++ src/services/data_processing.py
@@ -9,38 +9,32 @@ def retrieve_and_process_data(password: str, time: int):
 
         # Define user responses
         sheet_id = os.getenv("USER_DECISIONS_DATA")
-
+        if not os.path.exists(f'{password}.csv'):
+            pd.DataFrame(market_base)[['Nombre', 'Valor']].to_csv(f'{password}.csv', index=False)
+        # Convert the file to a pandas dataframe
+        current_prices = pd.read_csv(f'{password}.csv')
+            
         users_responses = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
         users_responses = users_responses[(users_responses["Contraseña"] == password) & (users_responses["Momento"] <= time)]
         users_responses.fillna('', inplace=True)
         users_responses.sort_values(by=['Nombre Usuario', 'Momento'], inplace=True)
 
-        # Process each user's investments
-        initial_capital = 1000
-        user_portfolios = {}
-
+        transactions = []
         for user in users_responses['Nombre Usuario'].unique():
             user_data = users_responses[users_responses['Nombre Usuario'] == user].sort_values(by='Momento')
-            portfolio = {'cash': initial_capital, 'holdings': {}, 'history': {}}
+            portfolio = {'cash': 1000, 'holdings': {}, 'history': {}}
 
             for idx, row in user_data.iterrows():
-                moment = row['Momento']
+                date = row['Marca temporal']
                 choices = [row['Empresa a Invertir'], row['Empresa 2 a Invertir']]
-                choices = [choice if choice != '' else 'Ninguna' for choice in choices]
-                time = int(moment)
 
                 # Update holdings based on choices
-                portfolio = update_portfolio(portfolio, choices, price_history.loc[time-1])
-
-                # Record portfolio value
-                total_value = portfolio['cash']
-                for company, shares in portfolio['holdings'].items():
-                    total_value += shares * price_history.loc[time + 1, company]
-                portfolio['history'][moment] = total_value
-
-            user_portfolios[user] = portfolio
-
-        return user_portfolios
+                portfolio = update_portfolio(users_responses['Nombre Usuario'], portfolio, date, choices, current_prices)
+                transactions += portfolio
+        
+        if not os.path.exists(f'{password}_participants.csv'):
+            pd.DataFrame(transactions).to_csv(f'{password}_participants.csv', index=False)
+        return transactions
     except Exception as e:
         print(e)
         raise e
