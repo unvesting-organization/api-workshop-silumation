e9e6fdfcbf98fb40fea429a45a8907c65f075efa
TeoEchavarria <echavarria.0042@gmail.com>
2024-11-05 06:49:41 -0500

    Little Changes Import

--- src/services/company_information.py
+++ src/services/company_information.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from src.utils.mongo_utils import MongoUtils
+
 
 async def companies_data(key, time):
     if time == 0:
@@ -8,6 +8,7 @@ async def companies_data(key, time):
         users_responses = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid=766765566")
         data_list = users_responses.to_dict(orient='records')
     else:
+        from src.utils.mongo_utils import MongoUtils
         documentos = await MongoUtils.find_document(f"{key}_company_{time}", {})
         data_list = [{k: v for k, v in doc.items() if k != '_id'} for doc in documentos]
     return data_list
