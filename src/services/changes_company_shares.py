07aa8fc978152d4eac8f7f4aac08a237fdf67c2c
TeoEchavarria <echavarria.0042@gmail.com>
2024-11-05 06:48:54 -0500

    Try Except and filter by Nombre, Cambio

--- src/services/changes_company_shares.py
+++ src/services/changes_company_shares.py
@@ -2,10 +2,14 @@ import os
 import pandas as pd
 
 def changes_shares(time):
-    sheet_id = os.getenv("CHANGES_COMPANY_SHARES_DATA")
-    users_responses = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid=0")
-    users_responses.fillna('', inplace=True)
-    users_responses = users_responses[users_responses["Momento"] == time]
+    try:
+        sheet_id = os.getenv("CHANGES_COMPANY_SHARES_DATA")
+        users_responses = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid=0")
+        users_responses.fillna('', inplace=True)
+        users_responses = users_responses[users_responses["Momento"] == time]
 
-    data_list = users_responses.to_dict(orient='records')
-    return data_list
+        data_list = users_responses[["Nombre", "Cambio"]].to_dict(orient='records')
+        return data_list
+    except Exception as e:
+        print("Failed in changes_shares")
+        raise e
