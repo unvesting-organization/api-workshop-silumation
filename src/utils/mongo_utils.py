cbc76e9daab54247f8dd19c0d179c3cf4b01a3e1
TeoEchavarria <echavarria.0042@gmail.com>
2024-11-05 01:33:56 -0500

    Refactor mongo_utils.py to use MongoClient instead of AsyncIOMotorClient

--- src/utils/mongo_utils.py
+++ src/utils/mongo_utils.py
@@ -1,9 +1,8 @@
 import os
 import logging
-from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import MongoClient
 from bson import ObjectId
 from typing import Any, Dict, List, Optional
-from pymongo import UpdateOne
 import json
 
 # Configure logging
@@ -11,11 +10,11 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class MongoUtils:
-    client: Optional[AsyncIOMotorClient] = None
+    client: Optional[MongoClient] = None
     db: Optional[Any] = None
 
     @classmethod
-    async def connect(cls):
+    def connect(cls):
         if cls.client is None:
             uri = os.getenv('MONGODB_URI_CONNECTION')
             database_name = os.getenv('MONGODB_DATABASE_NAME')
@@ -24,10 +23,10 @@ class MongoUtils:
                 logger.error('Environment variables MONGODB_URI_CONNECTION and MONGODB_DATABASE_NAME must be set.')
                 raise EnvironmentError('Missing MongoDB configuration.')
 
-            cls.client = AsyncIOMotorClient(uri)
+            cls.client = MongoClient(uri)
             try:
                 # The 'ping' command is used to verify the connection
-                await cls.client.admin.command('ping')
+                cls.client.admin.command('ping')
                 cls.db = cls.client[database_name]
                 logger.info('Connected to MongoDB')
             except Exception as err:
@@ -43,14 +42,14 @@ class MongoUtils:
         :param kwargs: Optional configurations for the collection (e.g., capped, size, etc.).
         :raises ValueError: If the collection already exists.
         """
-        await cls.connect()
-        existing_collections = await cls.db.list_collection_names()
+        cls.connect()
+        existing_collections = cls.db.list_collection_names()
         if collection_name in existing_collections:
             logger.warning(f'Collection "{collection_name}" already exists.')
             return
 
         try:
-            await cls.db.create_collection(collection_name, **kwargs)
+            cls.db.create_collection(collection_name, **kwargs)
             logger.info(f'Collection "{collection_name}" created successfully.')
         except Exception as e:
             logger.error(f'Failed to create collection "{collection_name}".', exc_info=e)
@@ -64,69 +63,88 @@ class MongoUtils:
         :param collection_name: Name of the collection to check.
         :return: True if the collection exists, False otherwise.
         """
-        await cls.connect()
-        existing_collections = await cls.db.list_collection_names()
+        cls.connect()
+        existing_collections = cls.db.list_collection_names()
         exists = collection_name in existing_collections
         logger.debug(f'Collection "{collection_name}" exists: {exists}')
         return exists
 
     @classmethod
     async def insert_document(cls, collection: str, document: Dict) -> ObjectId:
-        await cls.connect()
-        result = await cls.db[collection].insert_one(document)
+        cls.connect()
+        result = cls.db[collection].insert_one(document)
         logger.debug(f'Document inserted with _id: {result.inserted_id}')
         return result.inserted_id
 
     @classmethod
-    async def insert_many_portfolios(cls, collection: str, documents: List[Dict]) -> List[ObjectId]:
-        await cls.connect()
-        if not documents:
-            logger.warning('No documents to insert.')
+    async def insert_many_portfolios(cls, collection: str, portfolios: Dict[str, Any]) -> List[ObjectId]:
+        """
+        Inserts multiple portfolio documents into the specified collection.
+
+        :param collection: The name of the collection.
+        :param portfolios: A dictionary where the key is the user_id and the value is a portfolio object.
+        :return: List of inserted ObjectIds.
+        """
+        cls.connect()
+        if not portfolios:
+            logger.warning('No portfolios to insert.')
             return []
-        # Convert UserPortfolio objects to dictionaries if necessary
-        def serialize_documents(documents):
+
+        # Convert portfolio objects to dictionaries
+        def serialize_portfolios(portfolios):
             result = []
-            for key, portfolio in documents.items():
+            for user_id, portfolio in portfolios.items():
                 aux = {
-                    "user_id": key,
+                    "user_id": user_id,
                     'balance': portfolio.balance,
-                    'holdings': dict(portfolio.holdings)  # Convertimos defaultdict a un dict
+                    'holdings': dict(portfolio.holdings)  # Convert defaultdict to dict
                 }
                 result.append(aux)
             return result
-        
-        json_data = serialize_documents(documents)
+
+        documents = serialize_portfolios(portfolios)
         try:
-            result = await cls.db[collection].insert_many(json_data)
+            result = cls.db[collection].insert_many(documents)
+            logger.debug(f'{len(result.inserted_ids)} portfolios inserted into "{collection}".')
+            return result.inserted_ids
         except Exception as e:
-            logger.error('Failed to insert documents duplicate')
-            return
-        logger.debug(f'{len(result.inserted_ids)} documents inserted into "{collection}".')
-        return result.inserted_ids
+            logger.error('Failed to insert portfolios.', exc_info=e)
+            return []
 
     @classmethod
-    async def insert_many_companies(cls, collection: str, documents: List[Dict]) -> None:
-        await cls.connect()
+    async def insert_many_companies(cls, collection: str, companies: List[Dict]) -> List[ObjectId]:
+        """
+        Inserts multiple company documents into the specified collection.
+
+        :param collection: The name of the collection.
+        :param companies: A list of company dictionaries to insert.
+        :return: List of inserted ObjectIds.
+        """
+        cls.connect()
+        if not companies:
+            logger.warning('No companies to insert.')
+            return []
+
         try:
-            result = await cls.db[collection].insert_many(documents)
+            result = cls.db[collection].insert_many(companies)
+            logger.debug(f'{len(result.inserted_ids)} companies inserted into "{collection}".')
+            return result.inserted_ids
         except Exception as e:
-            logger.error('Failed to insert documents duplicate')
-            return
-        logger.debug(f'{len(result.inserted_ids)} documents inserted into "{collection}".')
-        return result.inserted_ids
+            logger.error('Failed to insert companies.', exc_info=e)
+            return []
 
     @classmethod
     async def find_document(cls, collection: str, query: Dict) -> List[Dict]:
-        await cls.connect()
+        cls.connect()
         cursor = cls.db[collection].find(query)
-        documents = await cursor.to_list(length=None)
+        documents = list(cursor)
         logger.debug(f'Found {len(documents)} documents matching query in "{collection}".')
         return documents
 
     @classmethod
     async def delete_document(cls, collection: str, query: Dict) -> int:
-        await cls.connect()
-        result = await cls.db[collection].delete_many(query)
+        cls.connect()
+        result = cls.db[collection].delete_many(query)
         logger.debug(f'Deleted {result.deleted_count} documents from "{collection}".')
         return result.deleted_count
 
@@ -137,15 +155,16 @@ class MongoUtils:
 
         :return: A list of collection names.
         """
-        await cls.connect()
-        collections = await cls.db.list_collection_names()
+        cls.connect()
+        collections = cls.db.list_collection_names()
         logger.debug(f'Collections in database: {collections}')
         return collections
-    
+
     @classmethod
     async def close_connection(cls):
         if cls.client:
             cls.client.close()
+            cls.client = None  # Reset the client to None
             logger.info('Closed connection to MongoDB')
         else:
             logger.warning('No active connection to close')
