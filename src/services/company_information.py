98e3796b7ae2d2ffa85488a315e7756426e2de44
TeoEchavarria <echavarria.0042@gmail.com>
2024-11-06 21:57:27 -0500

    Refactor company_information.py to include company images in the data retrieval and calculation process

--- src/services/company_information.py
+++ src/services/company_information.py
@@ -17,6 +17,7 @@ async def companies_data(key, time):
             sheet_id = os.getenv("CHANGES_COMPANY_SHARES_DATA")
             users_responses = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid=766765566")
             previous_data = { doc['Nombre']: doc['Valor'] for doc in users_responses.to_dict(orient='records')}
+            users_responses = { doc['Nombre']: doc['Image'] for doc in users_responses.to_dict(orient='records')}
         else:
             previous_docs = await MongoUtils.find_document(f"{key}_company_{time-1}", {})
             previous_data = {doc['Nombre']: doc['Valor'] for doc in previous_docs if 'Nombre' in doc and 'Valor' in doc}
@@ -29,6 +30,7 @@ async def companies_data(key, time):
                 # Calcular el cambio porcentual
                 change_percentage = ((current_value - previous_value) / previous_value) * 100
                 company['Cambio'] = change_percentage
+                company['Image'] = users_responses[company['Nombre']]
             else:
                 company['Cambio'] = None  # En caso de no encontrar valor anterior, se asigna None
     return current_data
